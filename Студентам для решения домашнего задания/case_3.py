0f49dfe73be16e79255e16df9da2402a068ee05f
Aydyn79 <77226260+Aydyn79@users.noreply.github.com>
2022-03-03 00:51:33 +0700

    Add files via upload

--- Студентам для решения домашнего задания/case_3.py
+++ Студентам для решения домашнего задания/case_3.py
@@ -4,10 +4,10 @@ import yaml
 data = {'1-й': [10,20,30,40], '2-й': 1, '3-й': {'f':[10,20,30,40], 's': 1}}
 
 
-with open('file.yaml', 'w') as f_n:
+with open('file.yaml', 'w', encoding='utf-8') as f_n:
     yaml.dump(data, f_n, default_flow_style=False, allow_unicode = True)
 
-with open('file.yaml') as f_n:
+with open('file.yaml', encoding='utf-8') as f_n:
     f_n_content = yaml.safe_load(f_n)
 
 print(f_n_content == data)
